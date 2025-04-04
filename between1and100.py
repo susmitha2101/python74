1376467aa189f630473a684e1ec889a8010ec002
susmitha2101 <susmithareddy2101@gmail.com>
2025-04-04 14:47:02 +0530

    Add files via upload

---
+++ between1and100.py
@@ -0,0 +1,5 @@
+n=int(input("enter the number"))
+if n>=1 and n<=100:
+    print("the given number is between the 1and100")
+else:
+    print("the given number is not between the 1and100")
---
+++ even.py
@@ -0,0 +1,5 @@
+a=int(input("enter the number:"))
+if a%2==0:
+    print("the given number is even")
+else:
+    print("the given number is odd")
